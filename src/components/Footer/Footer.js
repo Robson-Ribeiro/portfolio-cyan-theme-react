b8482ac7743062c330b8d887883431dd1f90eebb
Robson <robsonribeirorra@gmail.com>
2024-05-24 21:47:35 -0300

    Footer view

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="thanks"><span>Thanks!!!</span></p>
+        </div>
+    )
+}
+
+export default Footer;
