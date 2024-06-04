8cbc88d500f9e181f1990bb88777b47844f625c2
Robson <robsonribeirorra@gmail.com>
2024-06-04 15:51:45 -0300

    Project component

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './Project.css';
 
+
 const Project = ({ project }) => {
+
+    const style = {
+        backgroundImage: `url(/${project.image})`,
+    }
 
     return (
         <div className="project-card">
-            <div className="project-image">{project.image}</div>
+            <div className="project-image" style={style}></div>
             <div className="project-title">{project.title}</div>
             <div className="project-description">{project.description}</div>
-            <div className="project-tecnologies">{project.tecnologies}</div>
+            <div className="project-tecnologies"><span>Tecnologies Used</span><div>{project.tecnologies}</div></div>
         </div>
     )
 }
